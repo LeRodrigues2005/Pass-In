efbf4c09fc8e7b087c258dde8242208b18ea4276
AnnonS4 <leticiadepatta@gmail.com>
2024-04-04 11:26:57 -0300

    first commit

--- PassIn.Api/obj/Debug/net8.0/PassIn.Api.AssemblyInfo.cs
+++ PassIn.Api/obj/Debug/net8.0/PassIn.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PassIn.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e313d675c44faa17c1de494a3e7e2f79100781b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb43377f1e0185dbf1d3095245735eeeb252c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("PassIn.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PassIn.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PassIn.Exceptions/obj/Debug/net8.0/PassIn.Exceptions.AssemblyInfo.cs
+++ PassIn.Exceptions/obj/Debug/net8.0/PassIn.Exceptions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PassIn.Exceptions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e313d675c44faa17c1de494a3e7e2f79100781b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb43377f1e0185dbf1d3095245735eeeb252c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("PassIn.Exceptions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PassIn.Exceptions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
